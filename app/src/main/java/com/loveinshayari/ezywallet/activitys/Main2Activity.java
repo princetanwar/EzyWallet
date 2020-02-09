a60e57b9857ab967d00661cfbea84e3f682f9510
princetanwar <ptanwar468@gmail.com>
2020-02-09 16:54:10 +0530

    some change made

--- app/src/main/java/com/loveinshayari/ezywallet/activitys/Main2Activity.java
+++ app/src/main/java/com/loveinshayari/ezywallet/activitys/Main2Activity.java
@@ -1,17 +1,22 @@
 package com.loveinshayari.ezywallet.activitys;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
 import com.loveinshayari.ezywallet.R;
@@ -21,6 +26,7 @@ public class Main2Activity extends AppCompatActivity implements NavigationView.O
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,38 +34,47 @@ public class Main2Activity extends AppCompatActivity implements NavigationView.O
         setContentView(R.layout.activity_main2);
         setSupportActionBar(toolbar);
         toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
 
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navview);
 
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.home, R.id.wallet, R.id.proflie, R.id.team)
+                .setDrawerLayout(drawerLayout)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
 
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+
+    @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.wallet:
-                Toast.makeText(this, "wallet", Toast.LENGTH_SHORT).show();
-                drawerLayout.closeDrawers();
-                break;
-            case R.id.proflie:
-                Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.team:
-                Toast.makeText(this, "team", Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+            case R.id.invite:
+                Toast.makeText(this, "invite", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.logout:
                 Toast.makeText(this, "logout", Toast.LENGTH_SHORT).show();
-
                 SharedPreferences preferences = getSharedPreferences("com.loveinshayari.ezywallet_login_status", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = preferences.edit();
-                editor.putString("login_status","off");
+                editor.putString("login_status", "off");
                 editor.apply();
-                startActivity(new Intent(Main2Activity.this,MainActivity.class));
+                startActivity(new Intent(Main2Activity.this, MainActivity.class));
                 finish();
                 break;
         }
@@ -67,7 +82,11 @@ public class Main2Activity extends AppCompatActivity implements NavigationView.O
     }
 
     @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawers();
+        }
+        else {
+        super.onBackPressed();}
     }
 }
---
+++ app/src/main/java/com/loveinshayari/ezywallet/nav_fragments/themFragment.java
@@ -0,0 +1,33 @@
+package com.loveinshayari.ezywallet.nav_fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.loveinshayari.ezywallet.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class themFragment extends Fragment {
+
+
+    public themFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_them, container, false);
+    }
+
+}
